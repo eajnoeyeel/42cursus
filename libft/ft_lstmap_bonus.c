086337c80076956f5e3d711ea2f3eea44ab13697
yeolee2 <yeolee2@student.42seoul.kr>
2023-03-25 19:44:24 +0900

    refactor: remove .vscode file from project directory

---
+++ libft/ft_lstmap_bonus.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 03:35:36 by yeolee2           #+#    #+#             */
+/*   Updated: 2023/03/25 19:32:52 by yeolee2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*res;
+	t_list	*new;
+	void	*tmp;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	res = NULL;
+	while (lst)
+	{
+		tmp = f(lst->content);
+		new = ft_lstnew(tmp);
+		if (!new)
+		{
+			del(tmp);
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, new);
+		lst = lst->next;
+	}
+	return (res);
+}
