9fc04a54c8db7d44e7da6de5187546c0a80069ba
yeolee2 <yeolee2@student.42seoul.kr>
2023-06-02 23:45:58 +0900

    feat: Add ft_printf and correct code indentation

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yeolee2 <yeolee2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:48:45 by yeolee2           #+#    #+#             */
-/*   Updated: 2023/05/18 02:31:31 by yeolee2          ###   ########.fr       */
+/*   Updated: 2023/06/02 23:40:04 by yeolee2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdarg.h>
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -27,6 +28,15 @@ int		ft_cntdig(long long n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
+int		ft_printf(const char *format, ...);
+int		ft_specifier(va_list ap, char c);
+int		ft_putchar(unsigned char c);
+int		ft_putstr(char *s);
+int		ft_putaddr(unsigned long long addr);
+int		ft_putnbr(long long n);
+int		ft_putupperhex(unsigned int n);
+int		ft_putlowerhex(unsigned int n);
+int		ft_putunsigned(unsigned int n);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
@@ -37,6 +47,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
+void	ft_bzero(void *s, size_t n);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
@@ -47,7 +58,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
